a1747dc6fc2deeff9cf03ea054157b36fe8ee781
tans105 <tanmayawasthi105@gmail.com>
2017-05-15 21:18:37 +0530

    init commit

---
+++ src/main/java/com/rentalmanager/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.rentalmanager.controller;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final Map<String, List<String>> userDb = new HashMap<>();
+
+    public UserController() {
+        userDb.put("tanmayawasthi105@gmail.com", Arrays.asList("user"));
+        userDb.put("mojha2701@gmail.com", Arrays.asList("user", "admin"));
+    }
+    
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public LoginResponse login(@RequestBody final UserLogin login)
+        throws ServletException {
+        if (login.name == null || !userDb.containsKey(login.name)) {
+            throw new ServletException("Invalid login");
+        }
+        return new LoginResponse(Jwts.builder().setSubject(login.name)
+            .claim("roles", userDb.get(login.name)).setIssuedAt(new Date())
+            .signWith(SignatureAlgorithm.HS256, "secretkey").compact());
+    }
+
+    @SuppressWarnings("unused")
+    private static class UserLogin {
+        public String name;
+        public String password;
+    }
+
+    @SuppressWarnings("unused")
+    private static class LoginResponse {
+        public String token;
+
+        public LoginResponse(final String token) {
+            this.token = token;
+        }
+    }
+}
