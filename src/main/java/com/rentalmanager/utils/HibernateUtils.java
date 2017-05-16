209a570d41cb31bf02821809936b989ecbb4b44c
tans105 <tanmayawasthi105@gmail.com>
2017-05-16 19:11:57 +0530

    Connection with Hibernate complete

---
+++ src/main/java/com/rentalmanager/utils/HibernateUtils.java
@@ -0,0 +1,41 @@
+package com.rentalmanager.utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HibernateUtils {
+
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
+	private static Logger logger = LoggerFactory.getLogger(HibernateUtils.class);
+
+	public static SessionFactory getSessionFactory() {
+		try {
+			if (sessionFactory == null) {
+				logger.debug("Creating 1st Instance of Session Factory");
+				Configuration configuration = new Configuration();
+				configuration.configure("com/rentalmanager/hibernate/hibernate.cfg.xml");
+
+				serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return sessionFactory;
+	}
+
+	public static void closeSession(Session session) {
+		if (session != null) {
+			session.close();
+			session = null;
+		}
+	}
+
+}
---
+++ src/main/java/com/rentalmanager/utils/QueryFilterCriterion.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ *    
+ * 	
+ * 	  Project Benq WebService, all source code and data files except images,
+ * 	  Copyright 2008-2015 Grit-Innovation Software Pvt. Ltd., India
+ * 	
+ * 	
+ *******************************************************************************/
+package com.rentalmanager.utils;
+
+import java.io.Serializable;
+
+public class QueryFilterCriterion implements Serializable{
+	
+	
+	public static final String OPERATOR_EQUALS= " = " ;
+	public static final String OPERATOR_LESS_THAN=" < ";
+	public static final String OPERATOR_LESS_THAN_OR_EQUALS=" <= ";
+	public static final String OPERATOR_GREATER_THAN=" > ";
+	public static final String OPERATOR_GREATER_THAN_OR_EQUALS=" >= ";
+	public static final String OPERATOR_STARTS_WITH=" STARTS ";
+	public static final String OPERATOR_CONTAINS=" CONTAINS ";
+	public static final String OPERATOR_IN=" IN ";
+	public static final String OPERATOR_ALLOCATED= "IS";
+	
+	String attributeName;
+	String operator;
+	Object value;
+	
+	/**
+	 * Creates a new criterion with the specified
+	 * attribute name, operator and value.
+	 * 
+	 * @param attributeName the name of the attribute to be tested.
+	 * @param operator operator the operation, which must be one of the OPERATOR_ constants on the {@link com.dcs.applications.ObjectQueryCommand} class.
+	 * @param value the value to compare.
+	 */
+	public QueryFilterCriterion(
+			String attributeName,
+			String operator,
+			Object value){
+		this.attributeName = attributeName;
+		this.operator = operator;
+		this.value = value;
+	}
+
+	public String getAttributeName() {
+		return attributeName;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setAttributeName(String attributeName) {
+		this.attributeName = attributeName;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+}
