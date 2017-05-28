89307d5874b6bad29abe79905082da1f012a599f
tans105 <tanmayawasthi105@gmail.com>
2017-05-28 11:21:38 +0530

    module name @bug fixed

--- src/main/java/com/rentalmanager/tester/CustomUserIdTester.java
+++ src/main/java/com/rentalmanager/tester/CustomUserIdTester.java
@@ -12,7 +12,7 @@ public class CustomUserIdTester {
 		login.setHostelId("SSHHYDTEL0001");
 		login.setPassword("5f4dcc3b5aa765d61d8327deb882cf99");
 		login.setLastLoginDtm(new Date());
-		login.setRoleId(1);
+		login.setRoleId(2);
 		GenericDao dao=new GenericDao();
 		dao.saveOrUpdateEntity(login);
 	}
